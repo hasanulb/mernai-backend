215dc921cac4afe24b604790858ba18c999279a8
HASANUL BANNA M <63143372+hasanulb@users.noreply.github.com>
2026-01-06 10:37:25 +0530

    Update App.js

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.options("*", cors());
 
 // Routes
 app.use("/api", require("./routes/flow.routes"));
